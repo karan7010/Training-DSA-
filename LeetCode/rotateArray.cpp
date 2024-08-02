fe64fa2df21ab2c3e4835c7338fd6bfa9e7f58c5
karan7010 <124433927+karan7010@users.noreply.github.com>
2024-08-02 20:59:25 +0530

    rotateArray.cpp

---
+++ LeetCode/rotateArray.cpp
@@ -0,0 +1,33 @@
+
+/*189. Rotate Array
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]*/
+
+class Solution {
+public:
+    void rotate(std::vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    void reverse(std::vector<int>& nums, int start, int end) {
+        while (start < end) {
+            std::swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
+    }
+};
