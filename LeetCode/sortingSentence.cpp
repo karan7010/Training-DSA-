43d5805a3bbe9e1d626d63fa8142213c900e6d90
karan7010 <124433927+karan7010@users.noreply.github.com>
2024-08-02 21:04:41 +0530

    sortingSentence.cpp

---
+++ LeetCode/sortingSentence.cpp
@@ -0,0 +1,15 @@
+//Sorting the Sentence
+class Solution {
+public:
+    string sortSentence(string s) {
+        int n=s.size();
+    for(int i=0;i<n;i++)
+    {
+        for(j=n-i-1;j=0;j++)
+        {
+            if (s.[i][j]<s.[i+1][j])
+            
+        }
+    }
+    }
+};
